e5791472f611bbaadb67c1f5bb30822e6cffe46a
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-16 20:06:34 +0500

    start working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      HELLO WORLD
+    <div className="text-blue-600">
+      <Home />
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="w-screen h-screen flex justify-center items-center text-blue-600">
       Home
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
