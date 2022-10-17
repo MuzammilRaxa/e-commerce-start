60ea14ffa0df3d93f394bdc02fd165d43b557007
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-17 14:39:40 +0500

    change slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,20 +1,75 @@
-import React from "react";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
-import siled1 from "../helpers/img/c7.png";
+import React, { useEffect, useState } from "react";
+import { IoIosArrowBack, IoIosArrowForward} from "react-icons/io";
+import "./Slider.css";
+import { ApiSlides } from "../componentsApi/SliderApi";
+
 
 const Slider = () => {
+  // Styles
+  const slideStyle = "slide flex items-center justify-center h-[100%]";
   const arrowStyle =
-    "rounded-full hover:bg-slate-300  duration-500 ease-in-out flex justify-center items-center cursor-pointer";
+    "rounded-full bg-grey flex justify-center items-center shadow-md hover:cursor-pointer";
+
+  //States
+  const [slides] = useState(ApiSlides);
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  const prevSlide = () => {
+    if (activeSlide === 0) {
+      setActiveSlide(slides.length - 1);
+    } else {
+      setActiveSlide(activeSlide - 1);
+    }
+  };
+  const nextSlide = () => {
+    if (activeSlide === slides.length - 1) {
+      setActiveSlide(0);
+    } else {
+      setActiveSlide(activeSlide + 1);
+    }
+  };
+const slide = slides[0]
+console.log(slide.src)
   return (
-    <div className="w100% h-[500px] flex justify-between items-center  bg-white">
-      <div className={arrowStyle}>
-        <IoIosArrowBack style={{ fontSize: "30px" }} />
+    <div className="slider h-[540px] bg-white flex items-center justify-between mobile:hidden">
+      {/* left Arrow */}
+      <div className={arrowStyle} onClick={prevSlide}>
+        <IoIosArrowBack style={{ fontSize: "50px" }} />
       </div>
-      <div className="w-[100%] h-[450px] ">
-        <img src={siled1} alt="Chiken" className="h-[80%] " />
-      </div>
-      <div className={arrowStyle}>
-        <IoIosArrowForward style={{ fontSize: "30px" }} />
+
+      {/* Slide */}
+
+      {/* {slides.map((slide, index) => {
+        if (index === activeSlide) {
+          return ( */}
+            <div
+              className={
+                `wrapper flex w-[100%] h-[500px] items-center justify-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative
+                ${slide.background}`
+              }
+            >
+              <div className={slideStyle}>
+                <div className="flex-1 flex justify-center items-center h-[100%]">
+                  <img
+                    className=" h-[80%] object-cover"
+                    src={slide.src}
+                    alt="Chikan"
+                  />
+                </div>
+                <div className="discription flex flex-col flex-1 place-items-start justify-center ml-11">
+                  <h2 className="text-[55px]">{slide.content.h2}</h2>
+                  <p className=" text-[30px]">{slide.content.p}</p>
+                  <button className="btn">Odder Now</button>
+                </div>
+              </div>
+            </div>
+          {/* );
+        }
+      })} */}
+      {/* Right Arrow */}
+
+      <div className={arrowStyle} onClick={nextSlide}>
+        <IoIosArrowForward style={{ fontSize: "50px" }} />
       </div>
     </div>
   );
---
+++ src/componentsApi/SliderApi.js
@@ -0,0 +1,20 @@
+
+export const ApiSlides = [
+    {
+        src: "/food.png",
+        content: {
+            h2: "Amazing Deals of Eid Delicious",
+            p: "Upto 40% off",
+        },
+        background: 'bg-[#c6e400]'
+    },
+    {
+        src: "./food.png",
+        active: true,
+        content: {
+            h2: "Amazing Deals of Eid Delicious",
+            p: "Upto 40% off",
+        },
+        background: 'bg-[#ffd028]'
+    },
+]
