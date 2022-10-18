f28563c034a4983bf6d4ec1a3c19efa027175a87
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-18 21:11:13 +0500

    feat Product details

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,22 +4,20 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    
-      <div className="counter flex  text-2xl justify-between gap-3">
-        Quantity
-        <div className="w-[124px] shadow-md flex justify-between">
-          <div className="bg-[#8a4af3] text-white w-8 flex flex-1 items-center justify-center rounded-l-lg cursor-pointer">
-            <button>-</button>
-          </div>
-          <div className="w-8 flex flex-1 items-center justify-center border-t border-b border-[#8a4af3]">
-            {count}
-          </div>
-          <div className="bg-[#8a4af3] flex-1 text-white w-8 flex items-center justify-center rounded-r-lg cursor-pointer">
-            <button>+</button>
-          </div>
+    <div className="counter  flex  text-2xl justify-between gap-3">
+      Quantity
+      <div className="w-[124px] shadow-md flex justify-between">
+        <div className="bg-white text-textColor w-8 flex flex-1 items-center justify-center rounded-l-lg cursor-pointer">
+          <button>-</button>
+        </div>
+        <div className="w-8 flex flex-1 items-center justify-center  ">
+          {count}
+        </div>
+        <div className="bg-white flex-1 text-textColor w-8 flex items-center justify-center overflow-hidden rounded-r-lg cursor-pointer">
+          <button>+</button>
         </div>
       </div>
-    
+    </div>
   );
 }
 
